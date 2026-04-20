2a7d886d63912d3859bf1ddfe56366a216afe206
陈志鹏 <2381711037@qq.com>
2026-04-21 06:07:08 +0800

    序列化帖子创建时间->前端

--- src/main/java/com/example/commentapp/entity/Post.java
+++ src/main/java/com/example/commentapp/entity/Post.java
@@ -2,6 +2,7 @@ package com.example.commentapp.entity;
 
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
+import com.fasterxml.jackson.annotation.JsonFormat;;
 
 @Entity
 @Table(name = "post")
@@ -11,6 +12,7 @@ public class Post {
     private Integer id;
     private String title;
     private String content;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime createTime = LocalDateTime.now();
     private String username;
 
